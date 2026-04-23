d37c4efcb6f9f0fde8898a7151a834a15581c301
Alfred Emsenhuber <fredi.emsenhuber@live.at>
2026-04-23 11:50:44 +0200

    Added more error handling to create endpoint

--- src/main/kotlin/com/example/demo/EdgeController.kt
+++ src/main/kotlin/com/example/demo/EdgeController.kt
@@ -1,6 +1,7 @@
 package com.example.demo
 
 import com.example.demo.dto.EdgeDto
+import com.example.demo.dto.ErrorDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -24,13 +25,19 @@ class EdgeController {
 
     // TODO: Have to do some more validation and also better Exception handling
     @PostMapping("/create")
-    fun createEdge(@RequestBody edge: EdgeDto): ResponseEntity<EdgeDto> {
-        val result = edgeRepository.createEdge(edge)
+    fun createEdge(@RequestBody edge: EdgeDto): ResponseEntity<Any> {
+        try {
+            val result = edgeRepository.createEdge(edge)
 
-        if (result > 0)
-            return ResponseEntity.status(HttpStatus.CREATED).body(edge)
+            if (result > 0)
+                return ResponseEntity.status(HttpStatus.CREATED).body(edge)
 
-        return ResponseEntity.status(HttpStatus.CONFLICT).build()
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(ErrorDto("Edge from ${edge.fromId} to ${edge.toId} already exists"))
+        } catch (ex: Exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorDto(ex.message ?: "An unexpected error occurred"))
+        }
     }
 
     @PostMapping("/delete")
